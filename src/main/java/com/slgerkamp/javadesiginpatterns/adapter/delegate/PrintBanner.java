3e4b5d8344d40e52c6a18202c750cf679a6fc521
hiroo takaku <slgerkamp@gmail.com>
2014-05-28 22:51:10 +0900

    Adapterパターン（Wrapperパターン）：アダプター 委譲編

---
+++ src/main/java/com/slgerkamp/javadesiginpatterns/adapter/delegate/PrintBanner.java
@@ -0,0 +1,40 @@
+package com.slgerkamp.javadesiginpatterns.adapter.delegate;
+
+import com.slgerkamp.javadesiginpatterns.adapter.Banner;
+
+/**
+ * Adapterパターン（Wrapperパターン）：委譲編
+ * 
+ * すでに提供されているものがそのまま使えない場合、
+ * 必要な形に変換してから利用する
+ * 「すでに提供されているもの」「必要なもの」のズレを埋める
+ * 
+ * Banner：「すでに提供されているもの」
+ * Print ：「必要なもの」
+ *
+ * Adapter（アダプタ）
+ * Adaptee「すでに提供されているもの」を使って、
+ * Target「必要なもの」を満たす
+ */
+public class PrintBanner extends Print{
+
+	// すでに提供されているもの
+	private final Banner banner;
+
+	public PrintBanner(String string) {
+		banner = new Banner(string);
+	}
+
+	// 変換処理（アダプターまたはラップ）
+	public void printWeak() {
+		// すでに提供されているもの
+		banner.showWithParen();
+	}
+
+	// 変換処理（アダプターまたはラップ）
+	public void printStrong() {
+		// すでに提供されているもの
+		banner.showWithAster();
+	}
+
+}
