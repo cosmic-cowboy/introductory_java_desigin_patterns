2be0be27d0d8728dfc0d5b76a05bab8739437d8c
hiroo takaku <slgerkamp@gmail.com>
2014-05-28 23:01:15 +0900

    Adapterパターン（Wrapperパターン）：対象 必要なもの（委譲編）

---
+++ src/main/java/com/slgerkamp/javadesiginpatterns/adapter/delegate/Print.java
@@ -0,0 +1,16 @@
+package com.slgerkamp.javadesiginpatterns.adapter.delegate;
+
+/**
+ * Adapterパターン（Wrapperパターン）：委譲編
+ * 
+ * 「必要なもの」
+ *  Target(対象)
+ * 
+ */
+public abstract class Print {
+
+	public abstract void printWeak();
+
+	public abstract void printStrong();
+
+}
